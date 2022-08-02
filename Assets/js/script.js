9704dcc7824957e90e948bae15246bb68b46bb4a
Cristina <cristinayunesm@gmail.com>
2022-08-02 01:25:23 -0400

    Third commit, removed questions from the HTML file and placed them inside the JS file. Also, shuffle questions function is now showing

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,56 +1,227 @@
-const startButton = document.getElementById('start-btn')
-const questionList = document.getElementById('all-questions')
-const footerArea = document.getElementById('footer')
+var startButton = document.getElementById('start-btn');
+var startDiv = document.getElementById('start-msg');
+var questionContainer = document.querySelector('#question-container');
+var startTimerBtn = document.getElementById('start-btn');
+var refreshButtonEl = document.getElementById('refresh-btn')
+var footerArea = document.querySelector('#footer')
+var questionElement = document.getElementById('question')
+var answerButtonElement = document.getElementById('answer-btns')
+// var answerCont = document.querySelector('#answerCont')
+// var answerBtn = document.createElement('button')
 
-var timerElement = document.querySelector("timer");
+var shuffledQuestions, currentQuestionIndex
 
-var timer;
-var startTimer;
+var score = 0; 
+
+// Timer 
+
+var remainingTime = 60;
+var isStopped = true;
+// Select Countdown container
+const countContainer = document.getElementById("timer");
+
+// function to display time
+const renderTime = () => {
+  // decement time
+  remainingTime -= 1;
+  // render count on the screen
+  countContainer.innerHTML = remainingTime;
+  // timeout on zero
+  if (remainingTime === 0) {
+    isStopped = true;
+    clearInterval(timer);
+    remainingTime = 60;
+  }
+};
+// Function to start Timer
+const startTimer = () => {
+  if (isStopped) {
+    isStopped = false;
+    countContainer.innerHTML = remainingTime;
+    timer = setInterval(renderTime, 1000);
+  }
+};
 
 
+// All questions
+
+var questions = [
+  {
+    question: 'Commonly used data types DO NOT include:',
+    answers: [
+      {text:'1. Strings', correct: false},
+      {text:'2. Booleans', correct: false},
+      {text:'3. Numbers', correct: false},
+      {text:'4. Alerts', correct: true}
+    ]
+  },
+  {
+    question: 'Which built-in method returns the length of the string?',
+    answers: [
+      {text:'1. length()', correct: true},
+      {text:'2. size()', correct: false},
+      {text:'3. index()', correct: false},
+      {text:'4. None of the above.', correct: false}
+    ]
+  },
+{
+  question: 'Which of the following function of String object returns the primitive value of the specified object.',
+  answers: [
+    {text:'1. toLocaleUpperCase()', correct: false},
+    {text:'2. toUpperCase()', correct: false},
+    {text:'3. toString()', correct: false},
+    {text:'4. valueOf()', correct: true}
+  ]
+},
+{
+  question: 'What is the meaning of isNaN?',
+  answers: [
+    {text:'1. Numbers and Nulls', correct: false},
+    {text:'2. Not a Number', correct: true},
+    {text:'3. Non a Number', correct: false},
+    {text:'4. Null Number', correct: false}
+  ]
+},
+{
+  question: 'Inside which HTML element do we put the JavaScript?',
+  answers: [
+    {text:'1. <script>', correct: true},
+    {text:'2. <scripting>', correct: false},
+    {text:'3. <js>', correct: false},
+    {text:'4. <javascript>', correct: false}
+  ]
+},
+{
+  question: 'Where is the correct place to insert a JavaScript?',
+  answers: [
+    {text:'1. <body>', correct: false},
+    {text:'2. <head>', correct: false},
+    {text:'3. both <head> and <body>', correct: true},
+    {text:'4. <footer>', correct: false}
+  ]
+},
+{
+  question: 'How do you create a function in JavaScript?',
+  answers: [
+    {text:'1. function myFunction()', correct: true},
+    {text:'2. function:myFunction()', correct: false},
+    {text:'3. function = myFunction()', correct: false},
+    {text:'4. let function()', correct: false}
+  ]
+},
+{
+  question: 'How can you add a comment in a JavaScript?',
+  answers: [
+    {text:'1. "This is a comment"', correct: false},
+    {text:'2. <!--This is a comment-->', correct: false},
+    {text:'//This is a comment', correct: true},
+    {text:'4. \\This is a comment', correct: false}
+  ]
+},
+{
+  question: 'What is the correct way to write a JavaScript array?',
+  answers: [
+    {text:'1. var color = ["red", "blue", "pink"]', correct: true},
+    {text:'2. var color = "red", "blue", "pink"', correct: false},
+    {text:'3. var color = ("red", "blue", "pink")', correct: false},
+    {text:'4. var color = {"red", "blue", "pink"}', correct: false}
+  ]
+},
+{
+  question: 'How do you declare a JavaScript variable?',
+  answers: [
+    {text:'1. v varName', correct: false},
+    {text:'2. var varName', correct: true},
+    {text:'3. variable varName', correct: false},
+    {text:'4. var = varName', correct: false}
+  ]
+},
+]
+
+
+// Start quizz event
 startButton.addEventListener('click', startQuiz)
 
+//To start timer by clicking event
+startTimerBtn.onclick = startTimer;
+
 function startQuiz() {
-    startButton.classList.add('hidden')
-    questionList.classList.remove('hidden')
-    footerArea.classList.remove('hidden')
-    timer = 120;
-    startTimer()
-  
-}
+    startButton.classList.add('hidden');
+    startDiv.classList.add('hidden'); 
+    questionContainer.classList.remove('hidden');
+    footerArea.classList.remove('hidden');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0
+    setNextQuestion()
 
+}
 function setNextQuestion() {
-
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer() {
-
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    var button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonElement.appendChild(button)
+  })
 }
-
-function startTimer() {
-    // Sets timer
-    timer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        // Tests if win condition is met
-        if (isWin && timerCount > 0) {
-          // Clears interval and stops timer
-          clearInterval(timer);
-          winGame();
-        }
-      }
-      // Tests if time has run out
-      if (timerCount === 0) {
-        // Clears interval
-        clearInterval(timer);
-        loseGame();
-      }
-    }, 1000);
-  }
-
-var refreshButtonEl = $('#refresh-btn');
-refreshButtonEl.on('click', function () {
-    location.reload();
-  });
   
+function resetState(){
+while (answerButtonElement.firstChild) {
+  answerButtonElement.removeChild
+  (answerButtonElement.firstChild)
+}
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+// Start Over button function
+refreshButtonEl.addEventListener("click", reload, false);
+function reload() {
+  reload = location.reload();
+}
+
+
+  // Possible code:
+  // questionElement.innerHTML = questions[currentQuestionIndex].question
+  //     for (let i = 0; i < questions[i].answers.length; i++ ) {
+  //      var answerBtn = document.createElement('button')
+  //     answerBtn.innerHTML = questions[currentQuestionIndex].answers[i].text
+  //     answerBtn.classList.add('btn')
+  //     answerCont.appendChild(answerBtn)
+  //     if (answers) {
+  //       answerBtn.dataset.true = answers.true
+  //     }
+  //     answerBtn.addEventListener('click', selectAnswer)
+  //     answerButtonsElement.appendChild(answerBtn)
+  //     console.log("buttons")
+  //   }

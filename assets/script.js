b30630f7d66184edd18a0ebe13b474ba575faa49
Cristina <cristinayunesm@gmail.com>
2022-08-04 22:42:14 -0400

    Score function is working, each correct will add a point to the final score

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,24 @@
-var startButton = document.getElementById('start-btn');
-var startDiv = document.getElementById('start-msg');
-var questionContainer = document.querySelector('#question-container');
-var startTimerBtn = document.getElementById('start-btn');
-var refreshButtonEl = document.getElementById('refresh-btn')
-var footerArea = document.querySelector('#time-cont')
-var questionElement = document.getElementById('question')
-var answerButtonElement = document.getElementById('answer-btns')
-var initialsEl = document.getElementById('initials');
-var submitScore = document.getElementById("submit");
+var startButton = document.getElementById("start-btn");
+var startDiv = document.getElementById("start-msg");
+var questionContainer = document.getElementById("container");
+var startTimerBtn = document.getElementById("start-btn");
+var refreshButtonEl = document.getElementById("refresh-btn");
+var timerArea = document.querySelector("#time-cont");
+var questionElement = document.getElementById("question");
+var answerButtonElement = document.getElementById("answer-btns");
+var initialsEl = document.getElementById("initials");
+var submitScore = document.getElementById("submit-btn");
+var finalScore = document.getElementById("final-score");
 
+
+var finalScreen = document.getElementById("final-screen");
 
 var shuffledQuestions;
 var currentQuestionIndex = 0;
 
-var score = 0; 
+var puntos = 0;
 
-// Timer 
+// Timer
 var remainingTime = 60;
 var isStopped = true;
 // Select Countdown container
@@ -43,202 +46,196 @@ const startTimer = () => {
   }
 };
 
-
 // List of all questions
 var questions = [
   {
-    question: 'Commonly used data types DO NOT include:',
+    question: "Commonly used data types DO NOT include:",
     answers: [
-      {text:'1. Strings', correct: false},
-      {text:'2. Booleans', correct: false},
-      {text:'3. Numbers', correct: false},
-      {text:'4. Alerts', correct: true}
-    ]
+      { text: "1. Strings", correct: false },
+      { text: "2. Booleans", correct: false },
+      { text: "3. Numbers", correct: false },
+      { text: "4. Alerts", correct: true },
+    ],
   },
   {
-    question: 'Which built-in method returns the length of the string?',
+    question: "Which built-in method returns the length of the string?",
     answers: [
-      {text:'1. length()', correct: true},
-      {text:'2. size()', correct: false},
-      {text:'3. index()', correct: false},
-      {text:'4. None of the above.', correct: false}
-    ]
+      { text: "1. length()", correct: true },
+      { text: "2. size()", correct: false },
+      { text: "3. index()", correct: false },
+      { text: "4. None of the above.", correct: false },
+    ],
   },
-{
-  question: 'Which of the following function of String object returns the primitive value of the specified object.',
-  answers: [
-    {text:'1. toLocaleUpperCase()', correct: false},
-    {text:'2. toUpperCase()', correct: false},
-    {text:'3. toString()', correct: false},
-    {text:'4. valueOf()', correct: true}
-  ]
-},
-{
-  question: 'What is the meaning of isNaN?',
-  answers: [
-    {text:'1. Numbers and Nulls', correct: false},
-    {text:'2. Not a Number', correct: true},
-    {text:'3. Non a Number', correct: false},
-    {text:'4. Null Number', correct: false}
-  ]
-},
-{
-  question: 'Inside which HTML element do we put the JavaScript?',
-  answers: [
-    {text:'1. <script>', correct: true},
-    {text:'2. <scripting>', correct: false},
-    {text:'3. <js>', correct: false},
-    {text:'4. <javascript>', correct: false}
-  ]
-},
-{
-  question: 'Where is the correct place to insert a JavaScript?',
-  answers: [
-    {text:'1. <body>', correct: false},
-    {text:'2. <head>', correct: false},
-    {text:'3. both <head> and <body>', correct: true},
-    {text:'4. <footer>', correct: false}
-  ]
-},
-{
-  question: 'How do you create a function in JavaScript?',
-  answers: [
-    {text:'1. function myFunction()', correct: true},
-    {text:'2. function:myFunction()', correct: false},
-    {text:'3. function = myFunction()', correct: false},
-    {text:'4. let function()', correct: false}
-  ]
-},
-{
-  question: 'How can you add a comment in a JavaScript?',
-  answers: [
-    {text:'1. "This is a comment"', correct: false},
-    {text:'2. <!--This is a comment-->', correct: false},
-    {text:'//This is a comment', correct: true},
-    {text:'4. \\This is a comment', correct: false}
-  ]
-},
-{
-  question: 'What is the correct way to write a JavaScript array?',
-  answers: [
-    {text:'1. var color = ["red", "blue", "pink"]', correct: true},
-    {text:'2. var color = "red", "blue", "pink"', correct: false},
-    {text:'3. var color = ("red", "blue", "pink")', correct: false},
-    {text:'4. var color = {"red", "blue", "pink"}', correct: false}
-  ]
-},
-{
-  question: 'How do you declare a JavaScript variable?',
-  answers: [
-    {text:'1. v varName', correct: false},
-    {text:'2. var varName', correct: true},
-    {text:'3. variable varName', correct: false},
-    {text:'4. var = varName', correct: false}
-  ]
-},
-]
+  {
+    question:
+      "Which of the following function of String object returns the primitive value of the specified object.",
+    answers: [
+      { text: "1. toLocaleUpperCase()", correct: false },
+      { text: "2. toUpperCase()", correct: false },
+      { text: "3. toString()", correct: false },
+      { text: "4. valueOf()", correct: true },
+    ],
+  },
+  {
+    question: "What is the meaning of isNaN?",
+    answers: [
+      { text: "1. Numbers and Nulls", correct: false },
+      { text: "2. Not a Number", correct: true },
+      { text: "3. Non a Number", correct: false },
+      { text: "4. Null Number", correct: false },
+    ],
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: "1. <script>", correct: true },
+      { text: "2. <scripting>", correct: false },
+      { text: "3. <js>", correct: false },
+      { text: "4. <javascript>", correct: false },
+    ],
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    answers: [
+      { text: "1. <body>", correct: false },
+      { text: "2. <head>", correct: false },
+      { text: "3. both <head> and <body>", correct: true },
+      { text: "4. <footer>", correct: false },
+    ],
+  },
+  // {
+  //   question: "How do you create a function in JavaScript?",
+  //   answers: [
+  //     { text: "1. function myFunction()", correct: true },
+  //     { text: "2. function:myFunction()", correct: false },
+  //     { text: "3. function = myFunction()", correct: false },
+  //     { text: "4. let function()", correct: false },
+  //   ],
+  // },
+  // {
+  //   question: "How can you add a comment in a JavaScript?",
+  //   answers: [
+  //     { text: '1. "This is a comment"', correct: false },
+  //     { text: "2. <!--This is a comment-->", correct: false },
+  //     { text: "//This is a comment", correct: true },
+  //     { text: "4. \\This is a comment", correct: false },
+  //   ],
+  // },
+  // {
+  //   question: "What is the correct way to write a JavaScript array?",
+  //   answers: [
+  //     { text: '1. var color = ["red", "blue", "pink"]', correct: true },
+  //     { text: '2. var color = "red", "blue", "pink"', correct: false },
+  //     { text: '3. var color = ("red", "blue", "pink")', correct: false },
+  //     { text: '4. var color = {"red", "blue", "pink"}', correct: false },
+  //   ],
+  // },
+  // {
+  //   question: "How do you declare a JavaScript variable?",
+  //   answers: [
+  //     { text: "1. v varName", correct: false },
+  //     { text: "2. var varName", correct: true },
+  //     { text: "3. variable varName", correct: false },
+  //     { text: "4. var = varName", correct: false },
+  //   ],
+  // },
+];
 
 // Start quizz event
-startButton.addEventListener('click', startQuiz)
+startButton.addEventListener("click", startQuiz);
 
 //To start timer by clicking event
 startTimerBtn.onclick = startTimer;
 
 function startQuiz() {
-    startButton.classList.add('hidden');
-    startDiv.classList.add('hidden'); 
-    questionContainer.classList.remove('hidden');
-    footerArea.classList.remove('hidden');
-    
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-  console.log(shuffledQuestions)
-    setNextQuestion()
+  startButton.classList.add("hidden");
+  startDiv.classList.add("hidden");
+  questionContainer.classList.remove("hidden");
+  timerArea.classList.remove("hidden")
+  
+
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  console.log(shuffledQuestions);
+  setNextQuestion();
 }
 
 function setNextQuestion() {
-  resetState()
-  console.log("Hello")
-  console.log(currentQuestionIndex)
-  showQuestion(shuffledQuestions[currentQuestionIndex+1])
-  // showQuestion()
-  console.log("next question")
+  resetState();
+  console.log("Hello");
+  console.log(currentQuestionIndex);
+  if (currentQuestionIndex === (questions.length-1)) {
+    submitScore.classList.remove("hidden");
+    startButton.classList.add("hidden");
+    startDiv.classList.add("hidden");
+    questionContainer.classList.add("hidden");
+    timerArea.classList.add("hidden");
+    finalScreen.classList.remove("hidden")
+   
+    finalScore.innerText = " " + puntos
+
+  } else {
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+  }
 }
+
 
 function showQuestion(question) {
-  console.log(question)
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    var button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
+  console.log(question);
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    var button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
 
-    answerButtonElement.appendChild(button)
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
     
 
-  })
-}
-// answerBtn.addEventListener('click', function(){
-//   console.log("next question2")
-// })
-
-
-//Attempting to generate questions without the shuffling criteria
-// function showQuestion() {
-// var currentQuestion = questions[currentQuestionIndex];
-// var questionElement = document.getElementById('question');
-// questionElement.textContent = currentQuestion.question;
-// answerButtonElement.innerHTML = '';
-// for (var i = 0; i < currentQuestion.answers.length; i++) {
-// var choice = currentQuestion.answers[i].text;
-// var choiceBtn = document.createElement('button');
-// choiceBtn.setAttribute('class', 'choice');
-// choiceBtn.setAttribute('value', choice);
-// answerButtonElement.appendChild(choiceBtn);
-// console.log(choice)
-// }
-// }
+    answerButtonElement.appendChild(button);
+  });
   
-function resetState() {
-  answerButtonElement.innerHTML = ""
-  // console.log(answerButtonElement.children)
+}
 
-// while (answerButtonElement.firstChild) {
-//   answerButtonElement.removeChild
-//   (answerButtonElement.firstChild)
-// }
+
+function resetState() {
+  answerButtonElement.innerHTML = "";
+
 }
 
 function selectAnswer(e) {
-  currentQuestionIndex +=1
-  console.log('Select Answer')
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  console.log(correct)
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
+  currentQuestionIndex += 1;
 
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+
+
+  if (correct) {
+   puntos+=1
+  }
+  console.log(correct);
+  Array.from(answerButtonElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
 }
+// Create a constant so my right and wrong colors show up
 
 function setStatusClass(element, correct) {
-  clearStatusClass(element)
+  clearStatusClass(element);
   if (correct) {
-    element.classList.add('correct')
+    element.classList.add("correct");
   } else {
-    element.classList.add('wrong');
-    remainingTime --- 5;
-  } 
-  setNextQuestion()
+    element.classList.add("wrong");
+    remainingTime-- - 5;
+  }
+  // setNextQuestion()
+  setTimeout(setNextQuestion, 1000);
 }
 
 function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
 }
 
 // Start Over button function -- working
@@ -247,38 +244,37 @@ function reload() {
   reload = location.reload();
 }
 
+// // Final Score Section
+// function saveHighscore() {
+//   // get value of input box
+//   var initials = initialsEl.value.trim();
 
-// Final Score Section
-function saveHighscore() {
-  // get value of input box
-  var initials = initialsEl.value.trim();
+//   if (initials !== "") {
+//     // get saved scores from localstorage, or if not any, set to empty array
+//     var highscores =
+//       JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-  if (initials !== "") {
-    // get saved scores from localstorage, or if not any, set to empty array
-    var highscores =
-      JSON.parse(window.localStorage.getItem("highscores")) || [];
+//     // format new score object for current user
+//     var newScore = {
+//       score: time,
+//       initials: initials,
+//     };
 
-    // format new score object for current user
-    var newScore = {
-      score: time,
-      initials: initials
-    };
+//     // save to localstorage
+//     highscores.push(newScore);
+//     window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
-    // save to localstorage
-    highscores.push(newScore);
-    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+//     // redirect to next page
+//     window.location.href = "score.html";
+//   }
+// }
 
-    // redirect to next page
-    window.location.href = "score.html";
-  }
-}
-
-function checkForEnter(event) {
-  // "13" represents the enter key
-  if (event.key === "Enter") {
-    saveHighscore();
-  }
-}
+// function checkForEnter(event) {
+//   // "13" represents the enter key
+//   if (event.key === "Enter") {
+//     saveHighscore();
+//   }
+// }
 
 // submit initials
-submitScore.onclick = saveHighscore;
+// submitScore.onclick = saveHighscore;
